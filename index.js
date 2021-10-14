0fceb9ec3506eac1ebe1d231b220bd67379b714d
Vic <vic@Vics-MacBook-Pro.local>
2021-10-14 12:36:15 -0500

    chore(config): test deploy heroku

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require('path');
 const scrapeIt = require("scrape-it")
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(express.static(path.join(__dirname, 'public'))).listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get('/express_backend', (req, res) => { 
 
@@ -44,4 +45,8 @@ app.get('/wong-test', async (req, res) => {
     product['tottus'] = scrapeTottus.data
   }
   res.send(product)
+});
+
+app.get('/', (req, res) => { 
+  res.sendFile(path.join(__dirname+'/public/index.html'));
 });
